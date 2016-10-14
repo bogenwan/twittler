9d8dd53c4fa003b00cabbd751ad8f698243c00c2
Johnny <po_bo@hotmail.com>
2016-10-14 03:45:34 -0700

    added buttons, implemented style and design, working on users tweet buttons function

---
+++ Twettler.js
@@ -0,0 +1,9 @@
+var getUserTweets = function(name) {
+  var result = [];
+for(var i = 0; i < streams.users; i++) {
+  if(streams.users === name){
+    result.push(tweetmessage);
+  }
+}
+  return result
+};
